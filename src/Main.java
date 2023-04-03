b10e2d2cd0d72b2ec465b8dea345a7857c7cce1d
mahfooz alam <mahfoozalam1070@gmail.com>
2023-04-03 22:00:13 +0530

    commit

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,10 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        //System.out.println("Hello world!");
+        int a = 5;
+         int b =17;
+         int  c = 45;
+         int sum = a + b + c;
+         System.out.println(sum);
     }
 }
